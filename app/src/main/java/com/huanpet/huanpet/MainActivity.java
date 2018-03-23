7ac4c670e0b39982726bd05e501249803e791c15
wl19930525 <958337798@qq.com>
2018-03-23 19:03:49 +0800

    第二次上传

--- app/src/main/java/com/huanpet/huanpet/MainActivity.java
+++ app/src/main/java/com/huanpet/huanpet/MainActivity.java
@@ -15,88 +15,16 @@ import com.umeng.socialize.media.UMImage;
 
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
-    private Button login;
-    private Button fenxiang;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+
 
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
-    }
 
-
-    UMShareListener umShareListener = new UMShareListener() {
-        @Override
-        public void onStart(SHARE_MEDIA platform) {
-            //分享开始的回调，可以用来处理等待框，或相关的文字提示
-        }
-
-        @Override
-        public void onResult(SHARE_MEDIA platform) {
-        }
-
-        @Override
-        public void onError(SHARE_MEDIA platform, Throwable t) {
-        }
-
-        @Override
-        public void onCancel(SHARE_MEDIA platform) {
-        }
-    };
-    UMAuthListener umAuthListener = new UMAuthListener() {
-        @Override
-        public void onStart(SHARE_MEDIA share_media) {
-
-        }
-
-        @Override
-        public void onComplete(SHARE_MEDIA share_media, int i, Map<String, String> map) {
-
-        }
-
-        @Override
-        public void onError(SHARE_MEDIA share_media, int i, Throwable throwable) {
-
-        }
-
-        @Override
-        public void onCancel(SHARE_MEDIA share_media, int i) {
-
-        }
-    };
-
-    private void initView() {
-        login = (Button) findViewById(R.id.login);
-        fenxiang = (Button) findViewById(R.id.fenxiang);
-
-        login.setOnClickListener(this);
-        fenxiang.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.login:
-                UMShareAPI umShareAPI = UMShareAPI.get(MainActivity.this);
-                umShareAPI.doOauthVerify(MainActivity.this, SHARE_MEDIA.QQ, umAuthListener);//第三个参数就是登录回调监听器
-                break;
-            case R.id.fenxiang:
-                UMImage image = new UMImage(MainActivity.this, R.mipmap.ic_launcher);
-                new ShareAction(MainActivity.this)
-                        .withText("hello")
-                        .withMedia(image)
-                        .setDisplayList(SHARE_MEDIA.QQ, SHARE_MEDIA.WEIXIN)
-                        .setCallback(umShareListener)
-                        .open();
-                break;
-        }
-    }
 }
