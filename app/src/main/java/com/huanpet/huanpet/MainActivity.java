d8ca7167b90fe4742ad9716ffcfd6db8210f0461
wl19930525 <958337798@qq.com>
2018-03-23 19:15:59 +0800

    第一次上传

--- app/src/main/java/com/huanpet/huanpet/MainActivity.java
+++ app/src/main/java/com/huanpet/huanpet/MainActivity.java
@@ -1,17 +1,8 @@
 package com.huanpet.huanpet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-import com.umeng.socialize.ShareAction;
-import com.umeng.socialize.UMAuthListener;
-import com.umeng.socialize.UMShareAPI;
-import com.umeng.socialize.UMShareListener;
-import com.umeng.socialize.bean.SHARE_MEDIA;
-import com.umeng.socialize.media.UMImage;
 
 import java.util.Map;
 
