9afd9eb1ec98eda34d3391a9ea395cf2bfe9e22f
heheda <632307362@qq.com>
2018-03-27 13:34:39 +0800

    mayuqiang

--- app/src/main/java/com/huanpet/huanpet/view/activity/MainActivity.java
+++ app/src/main/java/com/huanpet/huanpet/view/activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.huanpet.huanpet.view.activity;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.huanpet.huanpet.R;
 import com.huanpet.huanpet.base.BaseActivity;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends BaseActivity {
+
+    private TextView mtext;
+
     @Override
     protected int initgetId() {
         return R.layout.activity_main;
@@ -21,6 +28,16 @@ public class MainActivity extends BaseActivity {
         setPersinal(true);
         setOrientate(true);
         setSearchImage(true);
+        mtext = findViewById(R.id.mtext);
+
+        mtext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent  in = new Intent(MainActivity.this,Add_PetsActivity.class);
+                startActivity(in);
+            }
+        });
+
     }
 
     @Override
