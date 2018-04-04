c4174685a96d30ac1a201f517fcc94f1d19db155
heheda <632307362@qq.com>
2018-04-04 09:24:01 +0800

    mayuqiang xiangji

--- app/src/main/java/com/huanpet/huanpet/view/activity/PersonalActivity.java
+++ app/src/main/java/com/huanpet/huanpet/view/activity/PersonalActivity.java
@@ -1,16 +1,313 @@
 package com.huanpet.huanpet.view.activity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Dialog;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.huanpet.huanpet.R;
+import com.huanpet.huanpet.base.BaseActivity;
+
+import java.io.File;
 
 
-public class PersonalActivity extends AppCompatActivity {
+public class PersonalActivity extends BaseActivity implements View.OnClickListener {
+
+    private RelativeLayout head, name, sex, date, phone, weixin, QQ, adress;
+    private View inflate;
+    private Button choosePhoto, cancel, takePhoto;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private Dialog dialog;
+    private Uri imageUri;
+    private ImageView mImg_head;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+        initView();
+        initDialog();
+    }
+
+    @Override
+    protected int initgetId() {
+        return R.layout.activity_personal;
+    }
+
+    @Override
+    protected void initData() {
+
+        initView();
+
+    }
+
+    public void initDialog() {
+        dialog = new Dialog(this, R.style.ActionSheetDialogStyle);
+        inflate = LayoutInflater.from(this).inflate(R.layout.personal_anim_dialog_layout, null);
+        choosePhoto = inflate.findViewById(R.id.Photo_album);
+        takePhoto = inflate.findViewById(R.id.Taking_pictures);
+        cancel = inflate.findViewById(R.id.btn_cancel);
+        choosePhoto.setOnClickListener(this);
+        takePhoto.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        dialog.setContentView(inflate);
+        Window dialogWindow = dialog.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.y = 20;
+        dialogWindow.setAttributes(lp);
+        dialog.show();
+    }
+
+
+    public void initView() {
+        head.findViewById(R.id.person_head_layout);
+        head.setOnClickListener(this);
+        sex.findViewById(R.id.person_sex_layout);
+        sex.setOnClickListener(this);
+        name.findViewById(R.id.person_name_layout);
+        name.setOnClickListener(this);
+        date.findViewById(R.id.person_date_layout);
+        date.setOnClickListener(this);
+        phone.findViewById(R.id.person_phone_layout);
+        phone.setOnClickListener(this);
+        weixin.findViewById(R.id.person_weixin_layout);
+        weixin.setOnClickListener(this);
+        QQ.findViewById(R.id.person_QQ_layout);
+        QQ.setOnClickListener(this);
+        adress.findViewById(R.id.person_address_layout);
+        adress.setOnClickListener(this);
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initAdapter() {
+
+    }
+
+    public void initTakePhoto() {
+
+
+        //创建file对象，用于存储拍照后的图片；
+        File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            imageUri = FileProvider.getUriForFile(PersonalActivity.this,
+                    "com.gyq.cameraalbumtest.fileprovider", outputImage);
+        } else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+
+        //启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+
+    }
+
+    //跳转到图库
+    public void openAlbum() {
+
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    //    判断请求权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "you denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+    }
+
+    //判断是否是Android4.4以上或以上。SDK大于19用不同的处理图片方法
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bm = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        mImg_head.setImageBitmap(bm);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {  //4.4及以上的系统使用这个方法处理图片；
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);  //4.4及以下的系统使用这个方法处理图片
+                    }
+                }
+            default:
+                break;
+        }
+
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imagePath) {
+
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            mImg_head.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    /**
+     * 4.4及以上的系统使用这个方法处理图片
+     *
+     * @param data
+     */
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            //如果document类型的Uri,则通过document来处理
+            String docID = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docID.split(":")[1];     //解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/piblic_downloads"), Long.valueOf(docID));
+
+                imagePath = getImagePath(contentUri, null);
+
+            }
+
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            //如果是content类型的uri，则使用普通方式使用
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            //如果是file类型的uri，直接获取路径即可
+            imagePath = uri.getPath();
+
+        }
+
+        displayImage(imagePath);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.person_head_layout:
+
+                initDialog();
+
+                break;
+            case R.id.person_sex_layout:
+                break;
+            case R.id.person_name_layout:
+                break;
+            case R.id.person_date_layout:
+                break;
+            case R.id.person_phone_layout:
+                break;
+            case R.id.person_weixin_layout:
+                break;
+            case R.id.person_QQ_layout:
+                break;
+            case R.id.person_address_layout:
+                break;
+            case R.id.Taking_pictures:
+
+                initTakePhoto();
+                break;
+            case R.id.Photo_album:
+
+                if (ContextCompat.checkSelfPermission(PersonalActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(PersonalActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                } else {
+                    openAlbum();
+                }
+                break;
+            case R.id.btn_cancel:
+
+                dialog.dismiss();
+                break;
+
+
+        }
     }
 }
